bdd5bc5e164e5fca49fdc977b08228989607e736
Victor Bucataru <ping@devktor.com>
2019-05-02 08:24:16 +0300

    bash script for pdf exporting

--- import/import.sh
+++ import/import.sh
@@ -2,14 +2,34 @@
 
 tempDir="./temp"
 dataDir="./data"
-filePath="pdf/Baza-intrebari-Principiile-zborului.pdf"
+
+filePath=$1;
 fileName=$(basename -- "$filePath")
 fileName="${fileName%.*}"
 textFilePath="$tempDir/$fileName.txt"
-xmlFilePath="$dataDir/$fileName.xml"
+xmlFilePath=${2:-"$fileName.xml"} 
 
 NEWLINE=$'\\n'
 TAB='    '
+
+SerializeAnswers () {
+    str="\\\\n${TAB}${TAB}<answers>"
+    answers=$1
+    for i in "${answers[@]}"
+    do :
+        str+="\\\\n${TAB}${TAB}${TAB}<answer>$i</answer>"
+    done
+    str+="\\\\n${TAB}${TAB}</answers>"
+    echo -ne "$str"
+}
+
+
+SerializeQuestionText () {
+    echo -ne "\\\\n${TAB}${TAB}<text>$1</text>"
+}
+
+echo "source file $filePath"
+echo "destination file $xmlFilePath"
 
 mkdir -p $tempDir;
 mkdir -p $dataDir
@@ -22,27 +42,56 @@ fi
 xmlContent="<questions>";
 questionCount=0
 
-qEnding="$NEWLINE${TAB}${TAB}</answers>$NEWLINE${TAB}</question>$NEWLINE";
+qEnding="$NEWLINE${TAB}</question>";
+
+
+answers=()
+question=""
 
 while read line
 do
     echo -n "."
-    line=$(echo $line| sed 's/&/\&amp;/g; s/</\&lt;/g; s/>/\&gt;/g; s/"/\&quot;/g; s/'"'"'/\&#39;/g')
-    if [[ $line =~ ^?[0-9]+\. ]]; then
-        if [[ $questionCount > 0 ]]; then
-            xmlContent+=$qEnding;
-        fi
-        questionCount=$((questionCount + 1))
-        xmlContent+="$NEWLINE${TAB}<question>$NEWLINE${TAB}${TAB}<text>$line</text>$NEWLINE${TAB}${TAB}<answers>";
-    else
-        if [[ $line =~ ^[a-z]\. ]]; then
-            xmlContent+="$NEWLINE${TAB}${TAB}${TAB}<answer>$line</answer>"
-        fi
+    line=$(echo $line| sed 's/&/\&amp;/g; s/</\&lt;/g; s/>/\&gt;/g; s/"/\&quot;/g; s/'"'"'/\&#39;/g' | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//' -e 's/\n//')
+
+    if [ ${#line} -eq 0 ]; then
+        continue
     fi
 
+    if [[ $line =~ ^?[0-9]+\. ]]; then
+        question=$line
+        #flush previous answers
+        if [ ${#answers[@]} -gt 0 ]; then
+            xmlContent+=$(SerializeAnswers "${answers[@]}")
+            xmlContent+=$qEnding;
+        fi
+        answers=()
+    else
+        if [[ $line =~ ^[[:space:]]*[a-z](\.|\)) ]]; then
+            answers+=("$line")
+            if [ ${#question} -gt 0 ]; then
+                xmlContent+="$NEWLINE${TAB}<question>$(SerializeQuestionText "$question")";
+                question=""
+            fi
+        else
+            size=${#answers[@]}
+            if [ $size -gt 0 ]; then
+                answers[${#answers[@]}-1]+=" $line" 
+            else
+                if [ ${#question} -gt 0 ]; then
+                    question="$question $line"
+                fi
+            fi
+        fi
+    fi
 done < $textFilePath
-echo ""
+
+if [ ${#answers[@]} ]; then
+    xmlContent+=$(SerializeAnswers "${answers[@]}")
+fi
+
 xmlContent+=$qEnding
-xmlContent+="</questions>"
+xmlContent+="$NEWLINE</questions>"
+
+echo ""
 
 echo -ne "${xmlContent}" >$xmlFilePath
